b80d88b2159c42eb5c9a5411b5434cd8e1158aca
dmzubr <dmzubr@gmail.com>
2020-04-30 21:40:18 +0300

    Change docker build pipeline
    Output base image wuth packages in explicit image to avoid big build when need to update only target code

--- docker_build.bat
+++ docker_build.bat
@@ -1,2 +1,3 @@
+docker build -t docker-repo.cashee.ru/vad-service-base:latest -f ./Dockerfile-base .
 docker build -t docker-repo.cashee.ru/vad-service-prod:latest -f ./Dockerfile .
 docker push docker-repo.cashee.ru/vad-service-prod:latest
